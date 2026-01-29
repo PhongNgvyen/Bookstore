73672ee119ab521e32552b1d986a4abf4214de10
PhongNgvyen <bhp978@myy.haaga-helia.fi>
2026-01-29 21:06:41 +0200

    First commit

---
+++ src/main/java/bookstore/backend/controller/BookController.java
@@ -0,0 +1,14 @@
+package bookstore.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BookController {
+    @GetMapping("/index")
+    @ResponseBody
+    public String getBookDetails() {
+        return "Testi";
+    }
+}
