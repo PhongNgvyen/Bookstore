809d71606b114f44977ef0ae3fbf8703279bcb05
PhongNgvyen <bhp978@myy.haaga-helia.fi>
2026-02-06 13:51:04 +0200

    Add and delete methods added

--- src/main/java/bookstore/backend/controller/BookController.java
+++ src/main/java/bookstore/backend/controller/BookController.java
@@ -4,8 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import bookstore.backend.domain.BookRepository;
+import bookstore.backend.domain.Book;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
 
 
 @Controller
@@ -18,5 +24,25 @@ public class BookController {
     public String getBookList(Model model) {
         model.addAttribute("books", repository.findAll());
         return "booklist";
+        }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id, Model model) {
+        repository.deleteById(id);
+        return "redirect:/booklist";
     }
+
+    @GetMapping("/add")
+    public String addBooks(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/save")
+    public String saveBooks(Book book) {
+        repository.save(book);
+        return "redirect:/booklist";
+    }
+
 }
+
--- src/main/java/bookstore/backend/domain/Book.java
+++ src/main/java/bookstore/backend/domain/Book.java
@@ -15,7 +15,7 @@ public class Book {
     private String author;
     private int publicationYear;
     private String isbn;
-    // private double price;
+
 
     public Book(){
 
@@ -26,21 +26,20 @@ public class Book {
     public String getAuthor() {return author;}
     public int getPublicationYear() {return publicationYear;}
     public String getIsbn() {return isbn;}
-    // public double getPrice() {return price;}
+
 
     public void setId(long id) { this.id = id;}
     public void setTitle(String title) {this.title = title;}
     public void setAuthor(String author) {this.author = author;}
     public void setPublicationYear(int publicationYear) {this.publicationYear = publicationYear;}
     public void setIsbn(String isbn) {this.isbn = isbn;}
-    // public void setPrice(double price) {this.price = price;}
+
 
     public Book(String title, String author, int publicationYear, String isbn) {
         this.title = title;
         this.author = author;
         this.publicationYear = publicationYear;
         this.isbn = isbn;
-        // this.price = price;
     }
 
 }
