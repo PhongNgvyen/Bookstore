2af2b691e1d1dd57ec5bf1eb5a422173816021b0
PhongNgvyen <bhp978@myy.haaga-helia.fi>
2026-02-26 20:38:09 +0200

    REST - delete and put methods

--- src/main/java/bookstore/backend/controller/BookController.java
+++ src/main/java/bookstore/backend/controller/BookController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -13,11 +14,12 @@ import bookstore.backend.domain.BookRepository;
 import bookstore.backend.domain.CategoryRepository;
 import bookstore.backend.domain.Book;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.ResponseEntity; 
 
 
 @Controller
@@ -85,4 +87,27 @@ public class BookController {
     public @ResponseBody Book saveBooksRest(@RequestBody Book book) {
     	return repository.save(book);
     }
+
+// RESTful service to update a book by its ID
+    @PutMapping("/books/{id}")
+    public ResponseEntity<Book> updateBookRest(@PathVariable Long id, @RequestBody Book updatedBook){
+        return repository.findById(id).map(book -> {
+            book.setTitle(updatedBook.getTitle());
+            book.setAuthor(updatedBook.getAuthor());
+            book.setIsbn(updatedBook.getIsbn());
+            book.setPublicationYear(updatedBook.getPublicationYear());
+            return ResponseEntity.ok(repository.save(book));
+        })
+        .orElse(ResponseEntity.notFound().build());
+    }
+
+// RESTful service to delete a book by its ID
+    @DeleteMapping("/books/{id}")
+    public ResponseEntity<?> deleteBookRest(@PathVariable Long id){
+        return repository.findById(id).map(book -> {
+            repository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
 }
